4cb37c0d2af25eef0100dd7a9574f2b65c4bdf3e
WesleyLeocadio <silvawesley374@gmail.com>
2020-10-09 09:18:19 -0300

    Adicionando comentarios

--- Fatorial.java
+++ Fatorial.java
@@ -3,7 +3,7 @@ public class Fatorial{
 	public static void main(String [] args){
 		System.out.println(calcularFatorial(5));	
 	}
-
+% comentario
 	public static int calcularFatorial(int n){
 		int resultado = 1;
 		for (int i =2; i<=n; i++)
